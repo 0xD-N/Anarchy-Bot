be058dd45d83caea5acc0b35358d8c3ead847435
0xD-N <63315159+0xD-N@users.noreply.github.com>
2022-08-25 21:06:38 -0500

    removed files

--- Cogs/CogList.py
+++
@@ -1,4 +0,0 @@
-def getCogs() -> list:
-    cogs = ["Cogs.FunCommands", "Cogs.Owner", "Cogs.Listeners", "Cogs.UtilityCommands", "Cogs.nasa"] 
-    
-    return cogs
--- Commands/DailyFact.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import time
-
-#just scrape the daily fact using dailyfacts.org
-
-#each category is seperated after the link. Science is dailyfacts.org/science, sports is dailyfacts.org/sports, and so on
-class Fact():
-    pass
